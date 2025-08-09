d507064073f4b9cd5b388c511ccdc8a2920b0591
priyanshu334 <99priyanshu33@gmail.com>
2025-08-09 23:19:13 +0530

    oo

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,8 +1,8 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest } from "next/server";
 
-import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import { createSupabaseServerClient } from "@/utils/supabase/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
@@ -11,7 +11,7 @@ export async function GET(request: NextRequest) {
   const next = searchParams.get("next") ?? "/";
 
   if (token_hash && type) {
-    const supabase = await createClient();
+    const supabase = await createSupabaseServerClient();
 
     const { error } = await supabase.auth.verifyOtp({
       type,
